9cbe4a49b9212285a8fc1d457cdb994170685618
alexpinkus <alexpinkus@hotmail.com>
2019-07-24 15:59:41 -0500

    Moved color picker component

--- src/app/Components/color-picker/color-picker.component.ts
+++ src/app/Components/color-picker/color-picker.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-color-picker',
   templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.css']
+  styleUrls: ['./color-picker.component.scss']
 })
 export class ColorPickerComponent {
   @Input() heading: string;
@@ -12,31 +12,47 @@ export class ColorPickerComponent {
 
   public defaultColors: string[] = [
     '#ffffff',
-    '#000105',
-    '#3e6158',
-    '#3f7a89',
-    '#96c582',
-    '#b7d5c4',
-    '#bcd6e7',
-    '#7c90c1',
-    '#9d8594',
-    '#dad0d8',
-    '#4b4fce',
-    '#4e0a77',
-    '#a367b5',
-    '#ee3e6d',
-    '#d63d62',
-    '#c6a670',
-    '#f46600',
-    '#cf0500',
-    '#efabbd',
-    '#8e0622',
-    '#f0b89a',
-    '#f0ca68',
-    '#62382f',
-    '#c97545',
-    '#c1800b'
+    '#ffff00',
+    '#ff00ff',
+    '#00ffff',
+    '#ff0000',
+    '#00ff00',
+    '#0000ff',
+    '#ffa500',
+    '#551a8b',
+    '#000000',
+
   ];
+
+
+  // public defaultColors: string[] = [
+  //   '#ffffff',
+  //   '#000105',
+  //   '#3e6158',
+  //   '#3f7a89',
+  //   '#96c582',
+  //   '#b7d5c4',
+  //   '#bcd6e7',
+  //   '#7c90c1',
+  //   '#9d8594',
+  //   '#dad0d8',
+  //   '#4b4fce',
+  //   '#4e0a77',
+  //   '#a367b5',
+  //   '#ee3e6d',
+  //   '#d63d62',
+  //   '#c6a670',
+  //   '#f46600',
+  //   '#cf0500',
+  //   '#efabbd',
+  //   '#8e0622',
+  //   '#f0b89a',
+  //   '#f0ca68',
+  //   '#62382f',
+  //   '#c97545',
+  //   '#c1800b'
+  // ];
+
   public show = false;
 
   constructor() { }
